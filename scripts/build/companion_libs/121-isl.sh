58a33fdd4f0995468622b97985c8c13785ed94ac
Milosch Meriac <milosch@picodilly.io>
2017-01-16 00:13:52 +0000

    redirect broken mirror

--- scripts/build/companion_libs/121-isl.sh
+++ scripts/build/companion_libs/121-isl.sh
@@ -14,7 +14,7 @@ if [ "${CT_ISL}" = "y" ]; then
 # Download ISL
 do_isl_get() {
     CT_GetFile "isl-${CT_ISL_VERSION}" \
-        http://isl.gforge.inria.fr
+        http://ftp.vim.org/languages/gcc/infrastructure
 }
 
 # Extract ISL
